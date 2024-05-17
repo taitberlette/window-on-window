d2c58f20f523c4324a48d04509e836b3d55cefbb
John-Matthew06 <jmhyslop1@gmail.com>
2024-05-17 10:46:07 -0400

    Started PauseMenu
    Made variables

--- src/States/PauseState.java
+++ src/States/PauseState.java
@@ -1,12 +1,23 @@
 package States;
 
+import Windows.ButtonWindow;
+
+import java.awt.*;
+
 public class PauseState extends State {
+    private ButtonWindow[] checkpoints;
+    private ButtonWindow home;
+    private ButtonWindow resume;
+    private ButtonWindow reset;
     public PauseState(StateManager stateManager) {
         super(stateManager);
+
     }
 
     public void open() {
-
+        for(int i = 0; i < 3; i++){
+            checkpoints[i].setVisible(true);
+        }
     }
 
     public void close() {
