8944477dbd4b636a40b919ffe721a07644eff12d
Alien-Boo123 <167790395+Alien-Boo123@users.noreply.github.com>
2024-05-22 10:59:01 -0400

    Added Entity Attributes and Sorted Player Images into one file

--- src/Game/GameObjects/Entities/Entity.java
+++ src/Game/GameObjects/Entities/Entity.java
@@ -1,6 +1,18 @@
 package Game.GameObjects.Entities;
 
 import Game.GameObjects.GameObject;
+import Game.Worlds.World;
+
+import java.awt.*;
 
 public abstract class Entity extends GameObject {
+
+    protected int health;
+    protected int maxSpeed;
+    protected double velocityX;
+    protected double velocityY;
+    protected double gravityAcceleration;
+    protected Rectangle bounds;
+    protected World world;
+
 }
