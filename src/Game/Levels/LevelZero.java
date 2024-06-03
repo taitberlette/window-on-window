d75f22eafa457552542cdc1c455ba1ebeb2bf4f1
Tait Berlette <54515877+taitberlette@users.noreply.github.com>
2024-06-03 19:43:30 -0400

    Add tutorial

--- src/Game/Levels/LevelZero.java
+++ src/Game/Levels/LevelZero.java
@@ -1,5 +1,6 @@
 package Game.Levels;
 
+import Assets.AssetManager;
 import Game.Game;
 import Game.GameObjects.Entities.Enemies.HellHound;
 import Game.GameObjects.Entities.Enemies.ShockSpider;
@@ -11,6 +12,11 @@ import Game.GameObjects.Gadgets.Target;
 import Game.GameObjects.Objects.Box;
 import Game.GameObjects.Objects.Door;
 import Game.GameObjects.Objects.HiddenNumber;
+import Game.Utilities.Ammunition;
+import Game.Utilities.Inventory;
+import Game.Worlds.CollisionType;
+import Game.Worlds.TerraWorld;
+import Windows.ImageWindow;
 import Windows.WorldWindow;
 
 import java.awt.*;
@@ -18,8 +24,37 @@ import java.util.Random;
 
 public class LevelZero extends Level {
 
+    private HiddenNumber firstNumber;
+    private Door door;
+    private Target target;
+    private MovingPlatform platform;
+    private BoxButton button;
+    private MovingWall wall;
+    private Box box;
+    private HellHound hellHound;
     private ShockSpider shockSpider;
-    private boolean unlocked = false;
+    private WorldWindow terraWorldWindow;
+    private WorldWindow etherWorldWindow;
+    private ImageWindow introTutorialWindow;
+    private ImageWindow skillsTutorialWindow;
+
+    private boolean skillsUnlocked = false;
+    private boolean seenDoor = false;
+    private boolean seenNumbers = false;
+
+    private enum IntroTutorialState {
+        MOVEMENT,
+        LADDER,
+        SWITCH_WINDOW,
+        POCKET_KNIFE,
+        THROW_BONE,
+        TUTORIAL_PAUSE_PLATFORM,
+        BOX,
+        TUTORIAL_PAUSE_WALL,
+        CODE
+    }
+
+    private IntroTutorialState tutorialState = IntroTutorialState.MOVEMENT;
 
     public LevelZero(Game game, Player player) {
         super(game, player, "Level_Tutorial");
@@ -32,47 +67,32 @@ public class LevelZero extends Level {
 
         int[] combination = {random.nextInt(1, 10)};
 
-        HiddenNumber firstNumber = new HiddenNumber(new Point(1750, 280), combination[0], Color.RED);
+        firstNumber = new HiddenNumber(new Point(1750, 280), combination[0], Color.RED);
         etherWorld.addGameObject(firstNumber);
 
-        Door door = new Door(new Point(1789, 255), player, terraWorld, game, "Level_Tutorial", combination);
+        door = new Door(new Point(1789, 255), player, terraWorld, game, "Level_Tutorial", combination);
         terraWorld.addGameObject(door);
 
-        Target target = new Target(new Point(890, 700));
+        target = new Target(new Point(890, 700));
         etherWorld.addGameObject(target);
 
-        MovingPlatform platform = new MovingPlatform(new Point(1078, 932), new Point(1078, 424), target);
+        platform = new MovingPlatform(new Point(1078, 932), new Point(1078, 424), target);
         etherWorld.addGameObject(platform);
 
-        BoxButton button = new BoxButton(new Point(1324, 352));
+        button = new BoxButton(new Point(1324, 352));
         button.setWorld(etherWorld);
         etherWorld.addGameObject(button);
 
-        MovingWall wall = new MovingWall(new Point(1432, 288), new Point(1432, 480), button);
+        wall = new MovingWall(new Point(1432, 288), new Point(1432, 480), button);
         etherWorld.addGameObject(wall);
 
-        Box box = new Box(new Point(250, 352));
+        box = new Box(new Point(250, 352));
         box.setWorld(etherWorld);
         etherWorld.addGameObject(box);
 
-        WorldWindow terraWorldWindow = new WorldWindow(terraWorld);
-        terraWorldWindow.setTarget(player);
-        terraWorldWindow.setFocusable(true);
-        terraWorldWindow.requestFocus();
-        terraWorldWindow.setKeyListener(game);
-        worldWindows.add(terraWorldWindow);
-
-        WorldWindow etherWorldWindow = new WorldWindow(etherWorld);
-        etherWorldWindow.setFocusable(true);
-        etherWorldWindow.requestFocus();
-        etherWorldWindow.setKeyListener(game);
-        etherWorldWindow.setLocation(new Point(911, 674));
-        worldWindows.add(etherWorldWindow);
-
-        HellHound hellHound = new HellHound();
+        hellHound = new HellHound();
         hellHound.setWorld(etherWorld);
         hellHound.setPlayer(player);
-//        hellHound.setLocation(new Point(362, 764));
         hellHound.setLocation(new Point(562, 764));
         etherWorld.addGameObject(hellHound);
 
@@ -82,16 +102,76 @@ public class LevelZero extends Level {
         shockSpider.setLocation(new Point(70, 305));
         etherWorld.addGameObject(shockSpider);
 
+        terraWorldWindow = new WorldWindow(terraWorld);
+        terraWorldWindow.setTarget(player);
+        terraWorldWindow.setFocusable(true);
+        terraWorldWindow.requestFocus();
+        terraWorldWindow.setKeyListener(game);
+        worldWindows.add(terraWorldWindow);
+
+        etherWorldWindow = new WorldWindow(etherWorld);
+        etherWorldWindow.setFocusable(true);
+        etherWorldWindow.requestFocus();
+        etherWorldWindow.setKeyListener(game);
+        etherWorldWindow.setLocation(new Point(911, 674));
+        worldWindows.add(etherWorldWindow);
+
+        introTutorialWindow = new ImageWindow("Tutorial", AssetManager.getImage("res\\Tutorial\\Movement.png"), new Point(110, 320));
+        introTutorialWindow.setKeyListener(game);
+        introTutorialWindow.setVisible(true);
+
+        skillsTutorialWindow = new ImageWindow("Skills", AssetManager.getImage("res\\Tutorial\\Skills.png"), new Point(81, 670));
+        skillsTutorialWindow.setKeyListener(game);
     }
 
     public void update(long deltaTime) {
         super.update(deltaTime);
 
-        if(shockSpider.getHealth() <= 0 && !unlocked) {
+        Point playerPosition = player.getLocation();
+        boolean inTerra = player.getWorld() instanceof TerraWorld;
+        boolean inEther = !inTerra;
+        Inventory inventory = player.getInventory();
+        CollisionType collisionType = player.getWorld().checkCollision(playerPosition);
+
+        // intro tutorial series
+        if(tutorialState == IntroTutorialState.MOVEMENT && collisionType == CollisionType.LADDER) {
+            tutorialState = IntroTutorialState.LADDER;
+            introTutorialWindow.changeImage(AssetManager.getImage("res\\Tutorial\\Ladder.png"));
+        } else if(tutorialState == IntroTutorialState.LADDER && terraWorldWindow.getBounds().intersects(etherWorldWindow.getBounds())) {
+            tutorialState = IntroTutorialState.SWITCH_WINDOW;
+            introTutorialWindow.changeImage(AssetManager.getImage("res\\Tutorial\\SwitchWorld.png"));
+        } else if(tutorialState == IntroTutorialState.SWITCH_WINDOW && inEther) {
+            tutorialState = IntroTutorialState.POCKET_KNIFE;
+            introTutorialWindow.changeImage(AssetManager.getImage("res\\Tutorial\\PocketKnife.png"));
+        } else if(tutorialState == IntroTutorialState.POCKET_KNIFE && inventory.hasItem(Ammunition.BONE)) {
+            tutorialState = IntroTutorialState.THROW_BONE;
+            introTutorialWindow.changeImage(AssetManager.getImage("res\\Tutorial\\Shoot.png"));
+        } else if(tutorialState == IntroTutorialState.THROW_BONE && target.isActivated()) {
+            tutorialState = IntroTutorialState.TUTORIAL_PAUSE_PLATFORM;
+            introTutorialWindow.setVisible(false);
+        } else if(tutorialState == IntroTutorialState.TUTORIAL_PAUSE_PLATFORM && player.getBounds().intersects(box.getBounds())) {
+            tutorialState = IntroTutorialState.BOX;
+            introTutorialWindow.changeImage(AssetManager.getImage("res\\Tutorial\\Box.png"));
+            introTutorialWindow.setLocation(new Point(55, 550));
+            introTutorialWindow.setVisible(true);
+        } else if(tutorialState == IntroTutorialState.BOX && button.isActivated()) {
+            tutorialState = IntroTutorialState.TUTORIAL_PAUSE_WALL;
+            introTutorialWindow.setVisible(false);
+        }
+
+        if(shockSpider.getHealth() <= 0 && !skillsUnlocked) {
             player.unlockFastLegs();
             player.unlockPhotosynthesis();
             player.unlockTunnelVision();
-            unlocked = true;
+            skillsUnlocked = true;
+            skillsTutorialWindow.setVisible(true);
         }
+    }
+
+    public void kill() {
+        super.kill();
+
+        introTutorialWindow.setVisible(false);
+        skillsTutorialWindow.setVisible(false);
     }
 }
