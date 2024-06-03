3de87f3e6b4c0af63e91fd5f334b7e02b973ceae
Tait Berlette <54515877+taitberlette@users.noreply.github.com>
2024-06-03 11:56:01 -0400

    Add door to end the level in the tutorial

--- src/Game/GameObjects/Objects/Door.java
+++ src/Game/GameObjects/Objects/Door.java
@@ -20,13 +20,17 @@ public class Door extends GameObject {
     private World world;
     private Game game;
     private BufferedImage doorImage;
+    private BufferedImage openDoorImage;
     private boolean inRange = false;
     private final double DOOR_DISTANCE = 128;
+    private final double WALK_IN_DISTANCE = 32;
+
     public Door(Point point, Player player, World world, Game game, String path, int[] combination) {
         setLocation(point);
 
         this.player = player;
         this.world = world;
+        this.game = game;
         this.combination = combination;
 
         numberCodeWindow = new NumberCodeWindow();
@@ -34,6 +38,7 @@ public class Door extends GameObject {
         numberCodeWindow.setKeyListener(game);
 
         doorImage = AssetManager.getImage("res\\" + path + "\\Door.png");
+        openDoorImage = AssetManager.getImage("res\\Objects\\OpenDoor.png");
     }
 
     public void update(long deltaTime) {
@@ -52,6 +57,13 @@ public class Door extends GameObject {
             numberCodeWindow.setVisible(inRange);
         }
 
+        double distanceHorizontal = Math.abs(playerPosition.getX() - (position.getX() + 64));
+
+        if(numberCodeWindow.isCorrect() && distanceHorizontal < WALK_IN_DISTANCE) {
+            System.out.println("COMPLETE LEVEL");
+            game.levelCompleted();
+        }
+
         Point windowLocation = new Point(playerPosition);
         windowLocation.translate(-512, -64);
 
@@ -61,7 +73,9 @@ public class Door extends GameObject {
     }
 
     public void draw(Graphics2D graphics2D) {
-        graphics2D.drawImage(doorImage, (int) position.getX(), (int) position.getY(), null);
+        BufferedImage door = (numberCodeWindow.isCorrect()) ? openDoorImage : doorImage;
+
+        graphics2D.drawImage(door, (int) position.getX(), (int) position.getY(), null);
     }
 
     public void keyPressed(KeyEvent e) {
@@ -76,5 +90,9 @@ public class Door extends GameObject {
         if(character >= '0' && character <= '9') {
             numberCodeWindow.typeNumber(character - '0');
         }
+    }
+
+    public void kill() {
+        numberCodeWindow.setVisible(false);
     }
 }
